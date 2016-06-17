f9af9d753c97a62e8e057898c3af39ff8726ac95
Daine.H <yifeng.huang@dianping.com>
2016-06-17 22:12:19 +0800

    1.login logout fuc

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='homepage'),
-    url(r'^index/$', views.index, name='homepage'),
+    url(r'^login/$', views.login_processor, name='login_processor'),
+    url(r'^logout/$', views.logout_processor, name='logout_processor')
 ]
--- home/views.py
+++ home/views.py
@@ -1,18 +1,37 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.conf import settings
+from django.contrib.auth import logout
 
 
 # Create your views here.
 
 
 def index(request):
+    access_entry = ("login", "logout")
     if request.method == 'GET':
         return render(request, 'home/index.html')
     if request.method == 'POST':
         app_name = request.POST.get('app_name')
+        if app_name in access_entry:
+            return redirect("/" + app_name)
         if app_name in settings.INSTALLED_APPS:
             return redirect("/" + app_name)
         else:
             context = {"error": "Invalid App"}
             return render(request, 'home/index.html', context)
+
+
+def login_processor(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated():
+            return redirect("/")
+        else:
+            return redirect("/admin")
+
+
+def logout_processor(request):
+    if request.user.is_authenticated():
+        logout(request)
+        return redirect("/")
+
